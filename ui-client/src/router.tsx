0fdc78ca3f669988af529d24038e2f07a1ecaf8b
Kinyabundi <kinyachristine14@gmail.com>
2024-11-10 16:11:46 +0700

    added project content page

--- ui-client/src/router.tsx
+++ ui-client/src/router.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import OverviewScreen from "./pages/OverviewScreen";
 import OverviewLayout from "./layouts/OverviewLayout";
+import NewProject from "./pages/NewProject";
+import ViewProject from "./pages/ProjectContent";
 
 const router = createBrowserRouter([
   {
@@ -9,12 +11,20 @@ const router = createBrowserRouter([
     element: <LandingPage />,
   },
   {
-    path: "/app",
+    path: "app",
     element: <OverviewLayout />,
     children: [
       {
         path: "",
         element: <OverviewScreen />,
+      },
+      {
+        path:"create-new-project",
+        element: <NewProject/>,
+      },
+      {
+        path:"view-project",
+        element: <ViewProject/>,
       },
     ],
   },

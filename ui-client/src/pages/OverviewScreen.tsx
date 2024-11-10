4dede39a1b7dbc0467cdf892d7feff9662b0292c
VinGitonga <gitongavincent64@gmail.com>
2024-11-10 10:32:07 +0300

    added overview tab content

--- ui-client/src/pages/OverviewScreen.tsx
+++ ui-client/src/pages/OverviewScreen.tsx
@@ -1,5 +1,25 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { AlbumIcon, BookOpen, StarIcon } from "lucide-react";
+
 const OverviewScreen = () => {
-  return <div>OverviewScreen</div>;
+  return (
+    <Tabs defaultValue="overview">
+      <TabsList>
+        <TabsTrigger value="overview">
+          <BookOpen className="w-5 h-5" /> <span className="ml-2">Overview</span>
+        </TabsTrigger>
+        <TabsTrigger value="repos">
+          <AlbumIcon className="w-5 h-5" /> <span className="ml-2">Repositories</span>
+        </TabsTrigger>
+        <TabsTrigger value="stars">
+        <StarIcon className="w-5 h-5" /> <span className="ml-2">Stars</span>
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="overview">Overview</TabsContent>
+      <TabsContent value="repos">Repositories</TabsContent>
+      <TabsContent value="stars">Stars</TabsContent>
+    </Tabs>
+  );
 };
 
 export default OverviewScreen;
